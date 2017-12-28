40134d6c95f7d5e7077c9bc9f962e742f0c79d65
richelbilderbeek <richel@richelbilderbeek.nl>
2017-12-28 19:24:12 +0100

    Codecov

---
+++ tests/testthat/test-get_paths.R
@@ -0,0 +1,19 @@
+context("get_paths")
+
+test_that("multiplication works", {
+
+  testthat::expect_equal(
+    length(
+      lumier::get_paths(
+        c(
+          "beast2_example_output.log",
+          "beast2_example_output.trees",
+          "beast2_example_output.xml",
+          "beast2_example_output.xml.state"
+        )
+      )
+     ),
+     4
+   )
+
+})
