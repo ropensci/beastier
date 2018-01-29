c37281cebfb340021710dfc09ff7f59f64756ed5
richelbilderbeek <richel@richelbilderbeek.nl>
2018-01-29 14:34:53 +0100

    run_beast2 works

--- R/run_beast2.R
+++ R/run_beast2.R
@@ -7,7 +7,7 @@
 run_beast2 <- function(
   input_filename,
   output_log_filename = "out.log",
-  output_trees_filename = "out.trees",
+  output_trees_filenames = "out.trees",
   output_state_filename = "out.xml.state",
   beast_jar_path = "~/Programs/beast/lib/beast.jar",
   verbose = FALSE
@@ -28,27 +28,38 @@ run_beast2 <- function(
     stop(
       "'input_filename' must be a valid BEAST2 XML file",
       "File '", input_filename, "' is not a valid BEAST2 file",
-      is_beast2_input_file(input_filename, verbose = verbose, beast_jar_path = beast_jar_path) # nolint internal function
+      is_beast2_input_file( # nolint internal function
+        input_filename,
+        verbose = verbose,
+        beast_jar_path = beast_jar_path)
     )
   }
 
   alignment_ids <- get_alignment_ids(input_filename)
-  # BEAST2 output file, containing the posterior parameter estimates
 
-  beast_log_filename <- paste0(base_filename, ".log")
+  if (length(output_trees_filenames) != length(alignment_ids)) {
+    stop(
+      "'output_trees_filenames' must have as much elements as ",
+      "'input_filename' has alignments. 'output_trees_filenames' has ",
+      length(output_trees_filenames), " elements, 'input_filename' has ",
+      length(alignment_ids), " alignments"
+    )
+  }
+
+  # BEAST2 output file, containing the posterior parameter estimates
+  beast_log_filename <- paste0(sort(alignment_ids)[1], ".log")
+
   # BEAST2 output file, containing the posterior phylogenies
-  beast_trees_filename <- paste0(base_filename, ".trees")
-  # BEAST2 output file, containing the final MCMC state
-  beast_state_filename <- paste0(base_filename, ".xml.state")
+  beast_trees_filenames <- paste0(alignment_ids, ".trees")
 
   # Run BEAST2 to measure posterior
   remove_files(
-    c(beast_state_filename, beast_log_filename, beast_trees_filename))
+    c(output_state_filename, beast_log_filename, beast_trees_filenames))
   testthat::expect_false(files_exist(
-    c(beast_state_filename, beast_log_filename, beast_trees_filename)))
+    c(output_state_filename, beast_log_filename, beast_trees_filenames)))
   cmd <- paste(
     "java -jar", beast_jar_path,
-    " -statefile ", beast_state_filename,
+    " -statefile ", output_state_filename,
     " -overwrite", input_filename
   )
   if (!verbose) {
@@ -59,14 +70,24 @@ run_beast2 <- function(
   if (verbose) {
     print(paste("beast exited with error code", exit_code))
     print(paste("beast_log_filename:", beast_log_filename))
-    print(paste("beast_trees_filename:", beast_trees_filename))
-    print(paste("beast_state_filename:", beast_state_filename))
+    print(paste("beast_trees_filenames:", beast_trees_filenames))
+    print(paste("output_state_filename:", output_state_filename))
     print(paste("file.exists(beast_log_filename):", file.exists(beast_log_filename)))
-    print(paste("file.exists(beast_trees_filename):", file.exists(beast_trees_filename)))
-    print(paste("file.exists(beast_state_filename):", file.exists(beast_state_filename)))
+    print(paste("file.exists(beast_trees_filenames):", file.exists(beast_trees_filenames)))
+    print(paste("file.exists(output_state_filename):", file.exists(output_state_filename)))
   }
   # If these are absent, BEAST2 could not parse the input file
-  testit::assert(file.exists(beast_state_filename))
+  testit::assert(file.exists(output_state_filename))
   testit::assert(file.exists(beast_log_filename))
-  testit::assert(file.exists(beast_trees_filename))
+  testit::assert(file.exists(beast_trees_filenames))
+
+  file.copy(from = beast_log_filename, to = output_log_filename, overwrite = TRUE)
+  for (i in seq_along(output_trees_filenames)) {
+    from <- beast_trees_filenames[i]
+    to <- output_trees_filenames[i]
+    file.copy(from = from, to = to, overwrite = TRUE)
+  }
+
+  file.remove(beast_log_filename)
+  file.remove(beast_trees_filenames)
 }
--- tests/testthat/test-run_beast2.R
+++ tests/testthat/test-run_beast2.R
@@ -1,15 +1,49 @@
 context("run_beast2")
 
-test_that("use", {
+test_that("single alignment creates all files", {
 
-  skip("WIP")
+  output_log_filename <- "tmp_single.log"
+  output_trees_filenames <- "tmp_single.trees"
+  output_state_filename <- "tmp_single.state"
+
+  output_files <- c(output_log_filename, output_trees_filenames, output_state_filename)
+  beastier:::remove_files(output_files)
+  testit::assert(!beastier:::files_exist(output_files))
+
   testthat::expect_silent(
-    run_beast2(get_path("anthus_2_4.xml"))
+    run_beast2(
+      input_filename = get_path("2_4.xml"),
+      output_log_filename = output_log_filename,
+      output_trees_filenames = output_trees_filenames,
+      output_state_filename = output_state_filename
+    )
   )
 
-  run_beast2(get_path("2_4.xml"), verbose = TRUE)
-
+  testthat::expect_true(beastier:::files_exist(output_files))
 })
+
+test_that("two alignments creates all files", {
+
+  output_log_filename <- "tmp_two.log"
+  output_trees_filenames <- c("tmp_two_a.trees", "tmp_two_b.trees")
+  output_state_filename <- "tmp_two.state"
+
+  output_files <- c(output_log_filename, output_trees_filenames, output_state_filename)
+  beastier:::remove_files(output_files)
+  testit::assert(!beastier:::files_exist(output_files))
+
+  testthat::expect_silent(
+    run_beast2(
+      input_filename = get_path("anthus_2_4.xml"),
+      output_log_filename = output_log_filename,
+      output_trees_filenames = output_trees_filenames,
+      output_state_filename = output_state_filename
+    )
+  )
+
+  testthat::expect_true(beastier:::files_exist(output_files))
+})
+
 
 test_that("abuse", {
 
@@ -29,6 +63,14 @@ test_that("abuse", {
       beast_jar_path = "abs.ent"
     ),
     "'beast_jar_path' must be the name of an existing file"
+  )
+
+  testthat::expect_error(
+    run_beast2(
+      input_filename = get_path("2_4.xml"),
+      output_trees_filenames = c("too", "much")
+    ),
+    "'output_trees_filenames' must have as much elements as 'input_filename' has alignments"
   )
 
 })
