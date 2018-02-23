a776f54e45eaa1f86c0e0d125dc9e7361f9bd38e
richelbilderbeek <richel@richelbilderbeek.nl>
2018-02-23 13:05:11 +0100

    @lintr-bot

--- tests/testthat/test-install_beast2.R
+++ tests/testthat/test-install_beast2.R
@@ -3,7 +3,7 @@ context("install_beast2")
 test_that("use", {
 
   folder_name <- tempdir()
-  beast_jar_path <- paste0(folder_name,"/beast/lib/beast.jar")
+  beast_jar_path <- paste0(folder_name, "/beast/lib/beast.jar")
   testit::assert(!file.exists(beast_jar_path))
   install_beast2(folder_name)
   testthat::expect_true(file.exists(beast_jar_path))
