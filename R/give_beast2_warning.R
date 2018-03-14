27b84eb534d3098848b96bd122d5f51ebfc49e2d
richelbilderbeek <richel@richelbilderbeek.nl>
2018-03-14 13:37:39 +0100

    Add 'give_beast2_warning' to detect BEAST2 warnings

--- R/give_beast2_warning.R
+++ R/give_beast2_warning.R
@@ -1,6 +1,51 @@
 #' Determines if BEAST2 issues a warning
 #' when using the BEAST2 XML input file
 #' @inheritParams default_params_doc
+#' @param filename name of the BEAST2 XML input file
+#' @param beast2_jar_path the path of \code{beast.jar}.
+#'   Use \link{get_default_beast2_jar_path} to get
+#'   the default BEAST jar file's path
+#' @return TRUE if the file produces a BEAST2 warning, FALSE if not
 #' @author Richel J.C. Bilderbeek
-give_beast2_warning <- function(beast2_input_file) {
+#' @seealso Use \code{\link{are_beast2_input_lines}} to check the lines
+#' @export
+give_beast2_warning <- function(
+  filename,
+  verbose = FALSE,
+  beast2_jar_path = get_default_beast2_jar_path()
+) {
+  if (!file.exists(filename)) {
+    stop(
+      "'filename' must be the name of an existing file. ",
+      "Filename '", filename, "' not found"
+    )
+  }
+  if (!file.exists(beast2_jar_path)) {
+    stop(
+      "'beast2_jar_path' must be the full path ",
+      "of the BEAST2 file 'beast.jar'. ",
+      "beast.jar not found at path '", beast2_jar_path, "'"
+    )
+  }
+
+  # Create the command to let BEAST2 validate the created XML file
+  cmd <- beastier::create_beast2_validate_cmd(
+    input_filename = filename,
+    beast2_jar_path = beast2_jar_path
+  )
+
+  # Valid BEAST2 input files will result in an output with 'Done!' at the
+  # last line
+  cmds <- unlist(strsplit(x = cmd, split = " "))
+  output <- system2(
+    cmds[1],
+    args = cmds[2:length(cmds)],
+    stdout = TRUE,
+    stderr = TRUE
+  )
+  if (verbose) {
+    print(output)
+  }
+
+  sum(grepl(x = output, pattern = "WARNING: ")) > 0
 }
--- tests/testthat/test-give_beast2_warning.R
+++ tests/testthat/test-give_beast2_warning.R
@@ -4,7 +4,7 @@ test_that("use", {
 
   testthat::expect_true(
     give_beast2_warning(
-      beautier:::get_beautier_paths("beast2_warning.xml")
+      filename = beastier:::get_beastier_path("beast2_warning.xml")
     )
   )
   testthat::expect_false(
@@ -12,4 +12,21 @@ test_that("use", {
       beautier:::get_beautier_paths("2_4.xml")
     )
   )
+})
+
+test_that("abuse", {
+
+  testthat::expect_error(
+    give_beast2_warning("abs.ent"),
+    "'filename' must be the name of an existing file. "
+  )
+
+  testthat::expect_error(
+    give_beast2_warning(
+      get_beastier_path("anthus_2_4.xml"),
+      beast2_jar_path = "abs.ent"
+    ),
+    "'beast2_jar_path' must be the full path of the BEAST2 file 'beast.jar'."
+  )
+
 })
