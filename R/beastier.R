3878080cc42e4ef22a2129de93980d0ce23f7065
richelbilderbeek <richel@richelbilderbeek.nl>
2018-06-01 12:28:57 +0200

    Add package documentation, thanks @bjoelle

---
+++ R/beastier.R
@@ -0,0 +1,13 @@
+#' beastier: A package to call BEAST2.
+#'
+#' 'beastier' allows to call BEAST2, a popular
+#' Bayesian phylogenetics tool, using
+#' an R interface. 'beastier' closely follows the interface
+#' of BEAST2, including its default settings.
+#'
+#' @seealso
+#'   The package \link[beautier]{beautier} can create
+#'   BEAST2 input files from R.
+#' @docType package
+#' @name beastier
+NULL
