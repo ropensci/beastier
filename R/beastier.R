91d52092e54dbb00ec6f3f9de016ba5629f6a7e2
richelbilderbeek <richel@richelbilderbeek.nl>
2018-06-01 12:49:44 +0200

    EOD

--- R/beastier.R
+++ R/beastier.R
@@ -6,8 +6,23 @@
 #' of BEAST2, including its default settings.
 #'
 #' @seealso
-#'   The package \link[beautier]{beautier} can create
-#'   BEAST2 input files from R.
+#' These are packages associated with 'beastier':
+#' \itemize{
+#'   \item{
+#'     The package \link[beautier]{beautier} can create
+#'     BEAST2 input files from R
+#'   }
+#'   \item{
+#'     The package \link[tracerer]{tracerer} can parse
+#'     BEAST2 output files from R
+#'   }
+#'   \item{
+#'     The package \link[babette]{babette} combines the
+#'     functionality of \link[beautier]{beautier},
+#'     \link[beastier]{beastier} and \link[tracerer]{tracerer}
+#'     into a single workflow
+#'   }
+#' }
 #' @docType package
 #' @name beastier
 NULL
