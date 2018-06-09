ede6c2964ec21a437161a385ba8def0a28b037d0
richelbilderbeek <richel@richelbilderbeek.nl>
2018-06-09 13:15:44 +0200

    rename to 'get_duplicate_param_ids' from 'get_duplicate_ids'

--- R/get_duplicate_param_ids.R
+++ R/get_duplicate_param_ids.R
@@ -7,13 +7,13 @@
 #'   line_1 <- "<parameter id=\"RealParameter.1\" ...</parameter>"
 #'   line_2 <- "<parameter id=\"RealParameter.2\" ...</parameter>"
 #'   testit::assert(
-#'     length(beastier:::get_duplicate_ids(c(line_1, line_2))) == 0)
+#'     length(beastier:::get_duplicate_param_ids(c(line_1, line_2))) == 0)
 #'   testit::assert(
-#'     beastier:::get_duplicate_ids(c(line_1, line_1)) == c("RealParameter.1"))
+#'     beastier:::get_duplicate_param_ids(c(line_1, line_1)) == c("RealParameter.1"))
 #'   testit::assert(
-#'     beastier:::get_duplicate_ids(c(line_2, line_2)) == c("RealParameter.2"))
+#'     beastier:::get_duplicate_param_ids(c(line_2, line_2)) == c("RealParameter.2"))
 #' @noRd
-get_duplicate_ids <- function(
+get_duplicate_param_ids <- function(
   text
 ) {
   if (!is.character(text)) {
--- R/has_unique_ids.R
+++ R/has_unique_ids.R
@@ -2,7 +2,7 @@
 #'   parameter IDs
 #' @param text the XML as text
 #' @return TRUE if all IDs are unique, FALSE otherwise
-#' @seealso to obtain the duplicate IDs, use \code{\link{get_duplicate_ids}}
+#' @seealso to obtain the duplicate IDs, use \code{\link{get_duplicate_param_ids}}
 #' @author Richel J.C. Bilderbeek
 #' @examples
 #'   line_1 <- "<parameter id=\"RealParameter.1\" ...</parameter>"
@@ -13,5 +13,5 @@
 has_unique_ids <- function(
   text
 ) {
-  length(get_duplicate_ids(text)) == 0 # nolint internal function
+  length(get_duplicate_param_ids(text)) == 0 # nolint internal function
 }
