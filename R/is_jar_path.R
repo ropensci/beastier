e001063abc5c99836640ef98cf50edf80782e881
richelbilderbeek <richel@richelbilderbeek.nl>
2018-09-13 10:02:12 +0200

    @lintr-bot

--- R/is_jar_path.R
+++ R/is_jar_path.R
@@ -4,10 +4,18 @@
 #' @return TRUE if the path is a path to a BEAST2 jar file
 #' @author Richel J.C. Bilderbeek
 #' @examples
-#'   testthat::expect_false(beastier:::is_jar_path("beast"))
-#'   testthat::expect_true(beastier:::is_jar_path("beast.jar"))
-#'   testthat::expect_false(beastier:::is_jar_path(get_default_beast2_bin_path()))
-#'   testthat::expect_true(beastier:::is_jar_path(get_default_beast2_jar_path()))
+#'   testthat::expect_false(
+#'     beastier:::is_jar_path("beast")
+#'   )
+#'   testthat::expect_true(
+#'     beastier:::is_jar_path("beast.jar")
+#'   )
+#'   testthat::expect_false(
+#'     beastier:::is_jar_path(get_default_beast2_bin_path())
+#'   )
+#'   testthat::expect_true(
+#'     beastier:::is_jar_path(get_default_beast2_jar_path())
+#'   )
 #' @noRd
 is_jar_path <- function(path) {
   !is.na(stringr::str_match(path, "(.*/)?beast\\.jar$")[1][1])
