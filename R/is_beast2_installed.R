e8ec80c060742b77498a270b950a13febfe0c1e4
richelbilderbeek <richel@richelbilderbeek.nl>
2018-09-17 09:03:10 +0200

    Fix 'uninstall_beast2'

--- R/is_beast2_installed.R
+++ R/is_beast2_installed.R
@@ -2,9 +2,9 @@
 #' @return TRUE if BEAST2 is installed
 #' @author Richel J.C. Bilderbeek
 #' @export
-is_beast2_installed <- function() {
-  bin_exists <- file.exists(get_default_beast2_bin_path())
-  jar_exists <- file.exists(get_default_beast2_jar_path())
+is_beast2_installed <- function(folder_name = get_default_beast2_folder()) {
+  bin_exists <- file.exists(file.path(folder_name, "beast", "bin", "beast"))
+  jar_exists <- file.exists(file.path(folder_name, "beast", "lib", "beast.jar"))
   testit::assert(bin_exists == jar_exists)
   jar_exists
 }
--- R/uninstall_beast2.R
+++ R/uninstall_beast2.R
@@ -1,11 +1,11 @@
 #' Uninstall BEAST2
 #' @author Richel J.C. Bilderbeek
 #' @export
-uninstall_beast2 <- function() {
-  if (!is_beast2_installed()) {
+uninstall_beast2 <- function(folder_name) {
+  if (!is_beast2_installed(folder_name)) {
     stop("Cannot uninstall absent BEAST2 at")
   }
   # STUB
-  file.remove(get_default_beast2_bin_path())
-  file.remove(get_default_beast2_jar_path())
+  file.remove(file.path(folder_name, "bin", "beast"))
+  file.remove(file.path(folder_name, "lib", "beast.jar"))
 }
