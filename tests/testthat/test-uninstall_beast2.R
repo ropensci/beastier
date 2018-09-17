8ec3e9e602499ef9e5a698a7fd8bad75d754de13
richelbilderbeek <richel@richelbilderbeek.nl>
2018-09-17 13:48:30 +0200

    Fix test

--- tests/testthat/test-uninstall_beast2.R
+++ tests/testthat/test-uninstall_beast2.R
@@ -2,9 +2,8 @@ context("uninstall_beast2")
 
 test_that("uninstall absent BEAST2 must throw", {
 
-  folder_name <- tempdir()
   expect_error(
-    uninstall_beast2(folder_name),
+    uninstall_beast2(folder_name = "/abs/ent"),
     "Cannot uninstall absent BEAST2 at"
   )
 })
