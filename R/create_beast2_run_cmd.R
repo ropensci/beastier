5e30679366bb9290f85d1041169db1c1bcc22299
richelbilderbeek <richel@richelbilderbeek.nl>
2018-10-19 15:16:45 +0200

    Do not check Java exe's name

--- R/create_beast2_run_cmd.R
+++ R/create_beast2_run_cmd.R
@@ -8,12 +8,11 @@
 #' @return a character vector with the command and
 #'   arguments to call BEAST2
 #' @examples
-#'   cmds <- create_beast2_run_cmd(
+#'   cmds <- beastier:::create_beast2_run_cmd(
 #'     input_filename = "input.xml",
 #'     output_state_filename = "output.xml.state",
-#'     beast2_path = get_default_beast2_jar_path()
+#'     beast2_path = beastier:::get_default_beast2_jar_path()
 #'   )
-#'   testit::assert(stringr::str_sub(cmds[1], -4) == "java")
 #'   testit::assert(cmds[2] == "-jar")
 #' @author Richel J.C. Bilderbeek
 #' @export
