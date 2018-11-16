b08b9db46af33947bb0274c33b690634ddadd882
richelbilderbeek <richel@richelbilderbeek.nl>
2018-11-16 12:33:58 +0100

    Added 'is_on_appveyor' and 'is_on_ci'

---
+++ R/is_on_ci.R
@@ -0,0 +1,23 @@
+#' Determines if the environment is AppVeyor
+#' @return TRUE if run on AppVeyor, FALSE otherwise
+#' @author Richel J.C. Bilderbeek
+#' @export
+is_on_appveyor <- function() {
+  Sys.getenv("APPVEYOR") != "" # nolint internal function
+}
+
+#' Determines if the environment is Travis CI
+#' @return TRUE if run on Travis CI, FALSE otherwise
+#' @author Richel J.C. Bilderbeek
+#' @export
+is_on_travis <- function() {
+  Sys.getenv("TRAVIS") != "" # nolint internal function
+}
+
+#' Determines if the environment is Travis CI
+#' @return TRUE if run on Travis CI, FALSE otherwise
+#' @author Richel J.C. Bilderbeek
+#' @export
+is_on_ci <- function() {
+  is_on_appveyor() || is_on_travis() # nolint internal function
+}
--- R/is_on_travis.R
+++
@@ -1,7 +0,0 @@
-#' Determines if the environment is Travis CI
-#' @return TRUE if run on Travis CI, FALSE otherwise
-#' @author Richel J.C. Bilderbeek
-#' @noRd
-is_on_travis <- function() {
-  Sys.getenv("TRAVIS") != ""
-}
