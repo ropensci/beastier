f5557938564e0dda168145829325faca4d1e1910
richelbilderbeek <richel@richelbilderbeek.nl>
2019-04-25 10:56:56 +0200

    Tests appear rock-solid

--- tests/testthat/test-run_beast2_from_options.R
+++ tests/testthat/test-run_beast2_from_options.R
@@ -21,7 +21,7 @@ test_that("abuse", {
   )
 })
 
-test_that("run_beast2 on local file in temp folder", {
+test_that("local file in temp folder", {
 
   if (!is_beast2_installed()) {
     skip("BEAST2 not installed")
@@ -31,17 +31,30 @@ test_that("run_beast2 on local file in temp folder", {
   tmp_wd <- tempfile(pattern = "beast2_tmp_folder")
   dir.create(tmp_wd)
   setwd(tmp_wd)
-  input_filename <- basename(
-    tempfile(pattern = "beast2_input_file", fileext = ".xml")
-  )
+
+  # All input and output files will be local
+  input_filename <- basename(tempfile(fileext = ".xml"))
+  output_log_filename <- basename(tempfile(fileext = ".log"))
+  output_trees_filename <- basename(tempfile(fileext = ".trees"))
+  output_state_filename <- basename(tempfile(fileext = ".xml.state"))
+
+  # Create input file locally
   file.copy(from = get_beastier_path("2_4.xml"), to = input_filename)
+
   expect_silent(
-    run_beast2_from_options(create_beast2_options(input_filename = input_filename))
+    run_beast2_from_options(
+      create_beast2_options(
+        input_filename = input_filename,
+        output_log_filename = output_log_filename,
+        output_trees_filenames = output_trees_filename,
+        output_state_filename = output_state_filename
+      )
+    )
   )
   setwd(cur_wd)
 })
 
-test_that("run_beast2 on full file in temp folder", {
+test_that("file with full path in temp folder", {
 
   if (!is_beast2_installed()) {
     skip("BEAST2 not installed")
