0b6d155c18d71c5725bf1fdeb118e078d1390818
richelbilderbeek <richel@richelbilderbeek.nl>
2019-04-25 10:21:39 +0200

    Add tests

--- tests/testthat/test-run_beast2_from_options.R
+++ tests/testthat/test-run_beast2_from_options.R
@@ -20,3 +20,40 @@ test_that("abuse", {
     run_beast2_from_options(beast2_options = "abs.ent")
   )
 })
+
+test_that("run_beast2 on local file in temp folder", {
+
+  if (!is_beast2_installed()) {
+    skip("BEAST2 not installed")
+  }
+
+  cur_wd <- getwd()
+  tmp_wd <- tempfile(pattern = "beast2_tmp_folder")
+  dir.create(tmp_wd)
+  setwd(tmp_wd)
+  input_filename <- basename(
+    tempfile(pattern = "beast2_input_file", fileext = ".xml")
+  )
+  file.copy(from = get_beastier_path("2_4.xml"), to = input_filename)
+  expect_silent(
+    run_beast2_from_options(create_beast2_options(input_filename = input_filename))
+  )
+  setwd(cur_wd)
+})
+
+test_that("run_beast2 on full file in temp folder", {
+
+  if (!is_beast2_installed()) {
+    skip("BEAST2 not installed")
+  }
+
+  cur_wd <- getwd()
+  tmp_wd <- tempfile(pattern = "beast2_tmp_folder")
+  dir.create(tmp_wd)
+  setwd(tmp_wd)
+  input_filename <- get_beastier_path("2_4.xml")
+  expect_silent(
+    run_beast2_from_options(create_beast2_options(input_filename = input_filename))
+  )
+  setwd(cur_wd)
+})
