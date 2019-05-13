d5f7a139893d77fad7203a8a29213589f32853e8
richelbilderbeek <richel@richelbilderbeek.nl>
2019-05-13 13:31:29 +0200

    Passes all local tests

--- tests/testthat/test-do_minimal_run.R
+++ tests/testthat/test-do_minimal_run.R
@@ -1,5 +1,10 @@
 context("test-do_minimal_run")
 
 test_that("use", {
+  if (!is_beast2_installed())
+  {
+    install_beast2()
+  }
+  testit::assert(is_beast2_installed())
   expect_silent(do_minimal_run())
 })
