53aa435299a89bd958fb063466177e26469bb90b
richelbilderbeek <richel@richelbilderbeek.nl>
2019-06-18 14:30:03 +0200

    Run tests that need BEAST2 installed only when BEAST2 is installed

--- tests/testthat/test-run_beast2.R
+++ tests/testthat/test-run_beast2.R
@@ -446,9 +446,7 @@ test_that("run_beast2 produces output", {
 
 test_that("run_beast2 with that cannot write to target directory", {
 
-  if (!is_beast2_installed()) {
-    return()
-  }
+  if (!is_beast2_installed()) return()
   expect_error(
     run_beast2(get_beastier_path("2_4.xml"), beast2_working_dir = "/"),
     paste0(
@@ -460,8 +458,12 @@ test_that("run_beast2 with that cannot write to target directory", {
 
 test_that("run_beast2 with invalid working directory", {
 
+  if (!is_beast2_installed()) return()
   expect_error(
-    run_beast2(get_beastier_path("2_4.xml"), beast2_working_dir = "/no/way"),
+    run_beast2(
+      input_filename = get_beastier_path("2_4.xml"),
+      beast2_working_dir = "/no/way"
+    ),
     paste0(
       "Cannot create working directory '/no/way' \n",
       "Maybe no permission to create it there?"
