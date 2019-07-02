9581e9a80936d41ab3f455c503cfce4fb552c63b
richelbilderbeek <richel@richelbilderbeek.nl>
2019-07-02 10:25:07 +0200

    Add check again

--- tests/testthat/test-is_beast2_input_file.R
+++ tests/testthat/test-is_beast2_input_file.R
@@ -86,13 +86,13 @@ test_that("abuse", {
     )
   )
 
-  # expect_error(
-  #   beastier::is_beast2_input_file(
-  #     get_beastier_path("anthus_2_4.xml"),
-  #     beast2_path = "BEAST.exe"
-  #   ),
-  #   "Cannot use the Windows executable BEAST2.exe in scripts"
-  # )
+  expect_error(
+    beastier::is_beast2_input_file(
+      get_beastier_path("anthus_2_4.xml"),
+      beast2_path = "BEAST.exe"
+    ),
+    "Cannot use the Windows executable BEAST2.exe in scripts"
+  )
 
 })
 
