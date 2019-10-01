a2b60fc49e4d6a97b0ce1d5532076d03d615eeab
richelbilderbeek <richel@richelbilderbeek.nl>
2019-10-01 16:11:30 +0200

    'upgrade_beast2' gives error when BEAST2 is not installed

--- tests/testthat/test-upgrade_beast2.R
+++ tests/testthat/test-upgrade_beast2.R
@@ -3,3 +3,18 @@ test_that("use", {
 
   expect_silent(upgrade_beast2())
 })
+
+
+test_that("give error when BEAST2 is not installed", {
+  if (!is_on_ci()) return()
+  if (!is_beast2_installed()) return()
+
+  testit::assert(is_beast2_installed())
+
+  beastier::uninstall_beast2()
+  expect_error(upgrade_beast2(), "BEAST2 is not installed")
+  beastier::install_beast2()
+
+  testit::assert(is_beast2_installed())
+})
+
