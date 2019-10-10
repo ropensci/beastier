9ddf6da95bcbfe0e86ff3cd88890cdd809911b9b
richelbilderbeek <richel@richelbilderbeek.nl>
2019-10-10 08:01:06 +0200

    Doc is complete

--- R/get_beast2_example_filename.R
+++ R/get_beast2_example_filename.R
@@ -2,12 +2,15 @@
 #'
 #' Will \link{stop} if the filename is not a BEAST2 example file
 #'
+#' @inheritParams default_params_doc
 #' @export
 get_beast2_example_filename <- function(
   filename,
-  folder = get_default_beast2_folder()
+  beast2_folder = get_default_beast2_folder()
 ) {
-  filenames <- beastier::get_beast2_example_filenames(folder = folder)
+  filenames <- beastier::get_beast2_example_filenames(
+    beast2_folder = beast2_folder
+  )
   index <- which(filename == basename(filenames))
   if (length(index) == 0) {
     stop(
--- R/get_beast2_example_filenames.R
+++ R/get_beast2_example_filenames.R
@@ -2,14 +2,14 @@
 #' BEAST2 example filenames
 #' @inheritParams default_params_doc
 #' @return a list with the full paths of all
-#' BEAST2 example filenames
+#'   BEAST2 example filenames
 #' @export
 get_beast2_example_filenames <- function(
-  folder = get_default_beast2_folder()
+  beast2_folder = get_default_beast2_folder()
 ) {
-  beast2_folder <- file.path(folder, "beast")
-  testit::assert(dir.exists(beast2_folder))
-  examples_folder <- file.path(beast2_folder, "examples")
+  beast2_subfolder <- file.path(beast2_folder, "beast")
+  testit::assert(dir.exists(beast2_subfolder))
+  examples_folder <- file.path(beast2_subfolder, "examples")
   testit::assert(dir.exists(examples_folder))
   list.files(
     path = examples_folder,
