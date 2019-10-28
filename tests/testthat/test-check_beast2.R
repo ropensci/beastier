b335adc8a71989de3d147024404de06448fc7382
richelbilderbeek <richel@richelbilderbeek.nl>
2019-10-28 10:28:59 +0100

    'check_beast2' is tested better

--- tests/testthat/test-check_beast2.R
+++ tests/testthat/test-check_beast2.R
@@ -4,7 +4,15 @@ test_that("use", {
 
   if (is_beast2_installed()) {
     expect_silent(check_beast2())
+    uninstall_beast2()
+    expect_error(check_beast2())
+    install_beast2()
+    expect_silent(check_beast2())
   } else {
+    expect_error(check_beast2())
+    install_beast2()
+    expect_silent(check_beast2())
+    uninstall_beast2()
     expect_error(check_beast2())
   }
 })
