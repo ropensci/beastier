2e14966961cf0e0f3efd6f48a1a83072f436ea28
richelbilderbeek <richel@richelbilderbeek.nl>
2020-07-28 13:04:57 +0200

    Update 'scripts/install_beast2.sh'

--- scripts/install_beast2.sh
+++ scripts/install_beast2.sh
@@ -7,12 +7,13 @@
 #
 # Usage:
 #
-#  ./scripts/install_beast2
+#  ./scripts/install_beast2.sh
 #
 #
 cat R/install_beast2.R > install_beast2.R
 cat R/check_os.R >> install_beast2.R
 cat R/get_default_beast2_download_url.R >> install_beast2.R
+cat R/get_default_beast2_version.R >> install_beast2.R
 echo "install_beast2()" >> install_beast2.R
 sed -i'.orginal' "s/beastier:://g" install_beast2.R
 cat install_beast2.R
