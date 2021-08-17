a5e6ae9cb531d6e8e03271241f262f9a29e4d5ab
richelbilderbeek <richel@richelbilderbeek.nl>
2021-08-17 13:07:49 +0200

    Fix script

--- scripts/build_singularity_container.sh
+++ scripts/build_singularity_container.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #
-# Create the Singularity container 'beastier.sif'
+# Build the Singularity container called `beastier.sif`
+# from the Singularity recipe `Singularity` (which is a default
+# name for a Singularity recipe)
 #
 # Usage:
 #
-#  ./scripts/create_singularity_container.sh
+# ./scripts/build_singularity_container.sh
 #
-singularity build --fakeroot beastier.sif Singularity
+#
+sudo -E singularity build beastier.sif Singularity
