9031c60facdc969c7db68985785976cf775724f4
richelbilderbeek <richel@richelbilderbeek.nl>
2021-09-02 16:30:38 +0200

    Wrote tests with Jeffrey Chang for #60

--- tests/testthat/test-create_beast2_options.R
+++ tests/testthat/test-create_beast2_options.R
@@ -2,6 +2,38 @@ test_that("use", {
   expect_silent(create_beast2_options())
 })
 
+test_that("increase stack space", {
+  skip("Issue 60")
+  # Result in same behavior
+  expect_silent(
+    create_beast2_options(
+      stack_size_kb = NA
+    )
+  )
+  # 320k stack size
+  expect_silent(
+    create_beast2_options(
+      stack_size_kb = 320
+    )
+  )
+  # Must be positive
+  expect_error(
+    create_beast2_options(
+      stack_size_kb = -1234567
+    )
+  )
+  expect_error(
+    create_beast2_options(
+      stack_size_kb = 0
+    )
+  )
+  expect_error(
+    create_beast2_options(
+      stack_size_kb = Inf
+    )
+  )
+})
+
 # BEAST2 working dir and folder for filenames must differ
 # is tested by test-check_beast2_options_filenames_not_in_working_dir.R
 
--- tests/testthat/test-create_beast2_run_cmd.R
+++ tests/testthat/test-create_beast2_run_cmd.R
@@ -9,6 +9,34 @@ test_that("minimal use", {
   )
 })
 
+test_that("minimal use", {
+  skip("Issue 60")
+  cmd <- create_beast2_run_cmd(
+    input_filename = "something",
+    output_state_filename = "something"
+  )
+  expect_equal(0, stringr::str_count(string = cmd, pattern = "-Xss"))
+
+
+ cmd <- create_beast2_run_cmd(
+    input_filename = "something",
+    output_state_filename = "something",
+    stack_size_kb = 2000
+  )
+  expect_equal(1, stringr::str_count(string = cmd, pattern = "-Xss"))
+  expect_true("-Xss2000k" %in% cmd)
+
+
+ cmd <- create_beast2_run_cmd(
+    input_filename = "something",
+    output_state_filename = "something",
+    stack_size_kb = 1234
+  )
+  expect_equal(1, stringr::str_count(string = cmd, pattern = "-Xss"))
+  expect_true("-Xss1234k" %in% cmd)
+
+})
+
 test_that("same results as create_beast2_run_cmd_from_options", {
   if (!is_beast2_installed()) return()
 
