5b184e0ae7d11011994225d75fe5390ff180a3d9
richelbilderbeek <richel@richelbilderbeek.nl>
2023-09-25 17:45:53 +0200

    Improve script

--- scripts/build_cran.sh
+++ scripts/build_cran.sh
@@ -3,19 +3,16 @@
 rm -rf build
 mkdir build
 
-cp -r ../R build
 cp -r ../inst build
 cp -r ../man build
+# cp -r ../Meta build
+cp -r ../R build
+# cp -r ../src build
 cp -r ../vignettes build
 cp ../DESCRIPTION build
 cp ../NAMESPACE build
 
-cd build
+cd build || exit 42
 R CMD build .
-R CMD check --as-cran $(ls *.tar.gz)
-
-
-if [[ $HOSTNAME == "N141CU" ]]; then
-  notify-send "Done building beastier"
-fi
-
+gz_files=$(ls ./*.tar.gz)
+R CMD check --as-cran "${gz_files}"
