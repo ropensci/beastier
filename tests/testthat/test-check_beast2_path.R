2f574d2821649905a514502476b50387fe90e7d4
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-07-08 10:43:46 +0200

    Improve codecov

--- tests/testthat/test-check_beast2_path.R
+++ tests/testthat/test-check_beast2_path.R
@@ -1,4 +1,10 @@
 test_that("use", {
+  expect_silent(
+    check_beast2_path(get_beastier_path("some path that exists TODO"))
+  )
+})
+
+test_that("abuse", {
   expect_error(
     check_beast2_path("abs.ent"),
     "File 'beast2_path' not found. Could not find file with path 'abs.ent'"
